db99e5291658c02549b71c2538f0c4840337d2dd
Padraig Donnelly <padraig@astropup.io>
2018-10-18 20:45:31 +0100

    Refactoring, dealing with possible null results from the API a bit more sensibly.

--- src/Aircraft.java
+++ src/Aircraft.java
@@ -1,16 +1,34 @@
-public class Aircraft {
+import org.json.JSONObject;
+
+class Aircraft {
 
     private String registration;
-    private String airline;
     private String departureAirport;
-    private int numberOfPassangers;
-    private float fuelRemaining;
+    private String destinationAirport;
+    private int altitude;
+    private float track;
+    private String latitude;
+    private String longitude;
 
-    public Aircraft(String registration, String airline, String departureAirport, int numberOfPassangers, float fuelRemaining) {
-        this.registration = registration;
-        this.airline = airline;
-        this.departureAirport = departureAirport;
-        this.numberOfPassangers = numberOfPassangers;
-        this.fuelRemaining = fuelRemaining;
+    private final String DELIMETER = ",";
+
+    Aircraft(JSONObject aircraft) {
+        this.registration = aircraft.has("Reg") ? aircraft.get("Reg").toString() : "Unknown";
+        this.departureAirport = aircraft.has("From") ? aircraft.get("From").toString().split(DELIMETER)[0].substring(5) : "Unknown";
+        this.destinationAirport = aircraft.has("To") ? aircraft.get("To").toString().split(DELIMETER)[0].substring(5) : "Unknown";
+        this.altitude = aircraft.has("Alt") ? Integer.parseInt(aircraft.get("Alt").toString()) : -1;
+        this.track = aircraft.has("Trak") ? Float.parseFloat(aircraft.get("Trak").toString()) : -1;
+        this.latitude = aircraft.has("Lat") ? aircraft.get("Lat").toString() : "Unknown";
+        this.longitude = aircraft.has("Long") ? aircraft.get("Long").toString() : "Unknown";
     }
+
+    @Override
+    public String toString() {
+        return this.registration +": "
+                + this.departureAirport +" - "+ this.destinationAirport
+                +" Position: "+ this.latitude +","+ this.longitude
+                +" Track: "+ this.track
+                +" Altitude: "+ this.altitude;
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,14 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.*;
+import java.net.ConnectException;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.security.InvalidParameterException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.Scanner;
 import java.util.stream.Stream;
@@ -16,9 +19,11 @@ public class Main {
     private static final String AIRPORT_LIST = "src/airports.csv";
     private static final String DELIMETER = ",";
     private static final int ICAO_INDEX = 5;
+    private static final int NAME_INDEX = 1;
     private static final int LATITUDE_INDEX = 6;
     private static final int LONGITUDE_INDEX = 7;
-    private static final String BASE_URL = ("https://public-api.adsbexchange.com/VirtualRadar/AircraftList.json?lat=%s&lng=%s&fDstL=0&fDstU=%d");
+    private static final String BASE_URL = ("https://public-api.adsbexchange.com/VirtualRadar/AircraftList.json?" +
+            "lat=%s&lng=%s&fDstL=0&fDstU=%d");
 
     /**
      * Returns the latitude and longitude of the airport matching the provided ICAO code.
@@ -26,7 +31,7 @@ public class Main {
      * @param airportCode The ICAO code of the airport.
      * @return The latitude and longitude as a comma separated string.
      */
-    private static String locateAirport(String airportCode) {
+    private static String locateAirport(String airportCode) throws IOException {
         try(Stream<String> stream = Files.lines(Paths.get(AIRPORT_LIST))){
             Optional<String> airport = stream.filter(line -> line
                     .split(DELIMETER)[ICAO_INDEX]
@@ -36,59 +41,95 @@ public class Main {
 
             if(airport.isPresent()) {
                 String[] tokens = airport.get().split(DELIMETER);
-                return tokens[LATITUDE_INDEX] + "," + tokens[LONGITUDE_INDEX];
+                return tokens[LATITUDE_INDEX] + "," + tokens[LONGITUDE_INDEX] +","+ tokens[NAME_INDEX];
             }
             return null;
-        } catch (IOException e) {
-            e.printStackTrace();
         }
-        return null;
     }
 
-    private static String findLocalAircraft(String latitude, String longitude, int range) {
+    /**
+     * Returns a list of Aircraft local to the passed-in latitude and longitude, or none if there aren't any.
+     *
+     * @param latitude Reference airport's latitude.
+     * @param longitude Reference airport's longitude.
+     * @param range Range (in KM) from the airport that we should look.
+     * @return List of Aircraft local to the passed-in latitude and longitude, or null if there aren't any.
+     */
+    private static List<Aircraft> findLocalAircraft(String latitude, String longitude, int range) throws IOException {
+
+        if(latitude == null || longitude == null) {
+            throw new InvalidParameterException("Latitude and longitude cannot be null.");
+        }
+
+        URL url = new URL(String.format(BASE_URL, latitude, longitude, range));
+        HttpURLConnection connection = getConfiguredConnection(url);
+
+        if(connection == null || connection.getResponseCode() != 200) {
+            throw new ConnectException("Bad response code from the ADSBExchange API.");
+        }
+
         String inputLine;
         StringBuilder response = new StringBuilder();
+        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
 
-        try {
-            URL url = new URL(String.format(BASE_URL, latitude, longitude, range));
-            HttpURLConnection connection = (HttpURLConnection)url.openConnection();
-
-            connection.setRequestMethod("GET");
-            connection.setRequestProperty("Content-Type", "application/json");
-            connection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
-            connection.setUseCaches(false);
-            connection.setDoOutput(true);
-
-            if(connection.getResponseCode() == 200) {
-                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-
-                while((inputLine = in.readLine()) != null) {
-                    response.append(inputLine);
-                }
-                in.close();
-
-                JSONObject obj = new JSONObject(response.toString());
-                JSONArray results = obj.getJSONArray("acList");
-                return results.toString();
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
+        while((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
         }
-        return null;
+        in.close();
+
+        JSONObject obj = new JSONObject(response.toString());
+        JSONArray localAircraftJSON = obj.getJSONArray("acList");
+
+        List<Aircraft> localAircraft = new ArrayList<>();
+        if (localAircraftJSON != null) {
+
+            for(int i = 0; i < localAircraftJSON.length(); i++) {
+                JSONObject aircraft = localAircraftJSON.getJSONObject(i);
+                localAircraft.add(new Aircraft(aircraft));
+            }
+        }
+        return localAircraft;
     }
 
-    public static void main(String[] args) {
+    /**
+     * Returns a fully configured HttpURLConnection object.
+     *
+     * @param url The API URL that we're trying to access.
+     * @return A fully configured HTTP URLConnection object.
+     */
+    private static HttpURLConnection getConfiguredConnection(URL url) throws IOException {
+        HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+        connection.setRequestMethod("GET");
+        connection.setRequestProperty("Content-Type", "application/json");
+        connection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 " +
+                "(KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
+        connection.setUseCaches(false);
+        connection.setDoOutput(true);
+
+        return connection;
+    }
+
+    public static void main(String[] args) throws IOException {
         System.out.println("Enter reference airport (ICAO): ");
         String referenceAirport = new Scanner(System.in).next();
 
         String location = locateAirport(referenceAirport);
-        System.out.println("Found airport at " + location);
 
-        String latitude = location.split(DELIMETER)[0];
-        String longitude = location.split(DELIMETER)[1];
+        String latitude = null;
+        String longitude = null;
 
-        System.out.println(findLocalAircraft(latitude, longitude, 100));
+        if (location != null) {
+            latitude = location.split(DELIMETER)[0];
+            longitude = location.split(DELIMETER)[1];
+        } else {
+            System.out.println("Invalid ICAO code.");
+        }
+
+        List<Aircraft> localAircraft = findLocalAircraft(latitude, longitude, 100);
+
+        for(Aircraft aircraft : localAircraft) {
+            System.out.println(aircraft.toString());
+        }
     }
 
 }
